cd7ecec423f73a694eb3919d4d953b613cd80529
vbth7777 <vbth7777@gmail.com>
2023-03-17 19:36:12 +0700

    17/03/2023

--- config.fish
+++ config.fish
@@ -6,6 +6,34 @@ if status is-interactive
 end
 if type -q exa
     alias ll "exa -l -g --icons"
+    alias ls "exa --icons"
     alias lla "ll -a"
+    alias t1 "exa -l -g --icons --tree --level 1"
+    alias t2 "exa -l -g --icons --tree --level 2"
+    alias t3 "exa -l -g --icons --tree --level 3"
+    alias t4 "exa -l -g --icons --tree --level 4"
     #. ~/.config/bashScripts/aliases.sh
 end
+#functions
+function repo-push
+  set REPO_PATH $argv[1] or (pwd)
+  set SCRIPT_PATH ~/.config/bspwm/scripts/push-commit.sh
+
+  cd $REPO_PATH
+
+  # Check if the push-commit.sh script exists
+  if test ! -f $SCRIPT_PATH; then
+    echo 'push-commit.sh script not found!'
+    return 1
+  end
+
+  # check if the current commit is the latest commit
+  if git remote update && git status -uno | grep -q 'Your branch is behind'
+    echo 'Your branch is behind the remote branch. Please pull changes before pushing.'
+    return 1
+  end
+
+  # run the push-commit.sh script
+  bash $SCRIPT_PATH
+end
+
